822078a3101e78b02f1d7efa35ccb0779c9be09d
Sagar Verma <8737sagar@gmail.com>
2024-07-16 23:14:12 +0530

    Add files via upload

---
+++ lab work/Q.1.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+
+	int x,y,z;
+	printf("Enter The Value Of x :");
+	scanf("%d",&x);
+	
+	printf("Enter The Value Of y :");
+	scanf("%d",&y);
+	
+	printf("Enter The Value Of z :");
+	scanf("%d",&z);
+	
+	if(x<y)
+	{
+		if(x<z)
+		{
+			printf("x is minimum");
+		}
+		else
+		{
+			printf("y is minimum");
+		}
+	}
+	else
+	{
+		if(y<z)
+		{
+			printf("y is minimum");
+		}
+		else
+		{
+			printf("z is minimum");
+		}
+	}
+}
