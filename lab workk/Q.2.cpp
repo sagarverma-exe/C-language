30aa8d87f414258add5daeb6d4581278864924b8
Sagar Verma <8737sagar@gmail.com>
2024-07-18 22:37:15 +0530

    Add files via upload

---
+++ lab workk/Q.2.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+main ()
+{
+	int x;
+	printf("Enter any Number : ");
+	scanf("%d",&x);
+	if(x>0)
+	{
+		printf("The number Is Positive");
+	}
+	
+	else if(x<0)
+	{
+		printf("The number Is Negative");
+	}
+	
+	else
+	{
+		printf("The number Is Neutral");
+	}
+}
