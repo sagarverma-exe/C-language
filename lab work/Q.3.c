396b69bd0ad666e87a0678c886643a6c96be0a3b
Sagar Verma <8737sagar@gmail.com>
2024-07-18 22:33:38 +0530

    Add files via upload

---
+++ lab work/Q.3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int x,y,z;
+	
+	printf("Enter the x number :");
+	scanf("%d",&x);
+	
+	printf("Enter the y number :");
+	scanf("%d",&y);
+	
+	printf("Enter the z number :");
+	scanf("%d",&z);        
+	
+	(x<y)?(x<z)?printf("x is maximum"):printf("z is maximum"):(y<z)?printf("y is maximum"):printf("z is maximum");
+	
+}
