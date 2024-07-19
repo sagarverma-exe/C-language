35c9fbfb1f78585d5e53b744bbb3cb3108a9cc04
Sagar Verma <8737sagar@gmail.com>
2024-07-19 11:53:42 +0530

    Add files via upload

---
+++ loops/Q.4.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+ main ()
+{
+
+int n;
+	
+	printf("enter your number:");
+	scanf("%d",&n);
+ 
+	while(n>=1)
+	
+	{
+
+	  
+     if(n%2==1)
+    {
+
+     printf("%d\n",n);
+
+ 	}
+	n--;
+    }
+
+}
---
+++ loops/Q.5.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+
+ main ()
+{
+
+int n;
+int a;
+	
+	printf("enter your n:");
+	scanf("%d",&a);
+ 
+	printf("enter your n:");
+	scanf("%d",&n);
+	
+	while(a<=n)
+	
+	{
+
+	  
+     if(a%4==0)
+    {
+
+     printf("%d\n",a);
+
+ 	}
+	a++;
+    }
+
+}
