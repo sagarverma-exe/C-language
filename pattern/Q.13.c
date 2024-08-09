af859e50c29c2d93a23b0446256aa2869c4760e2
Sagar Verma <8737sagar@gmail.com>
2024-08-09 12:15:30 +0530

    Add files via upload

---
+++ pattern/Q.13.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main()
+{
+	char i,j;
+	char ch='A';
+	
+	for(i=1; i<=5; i++)
+	{
+		for(j=1; j<=i;j++)
+		{
+			printf("%c ",ch);
+			ch++;	
+		}
+		printf("\n");
+	}
+	
+}
