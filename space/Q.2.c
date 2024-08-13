85cc232c89c0631b3f87cffa2980f5867efc77d6
Sagar Verma <8737sagar@gmail.com>
2024-08-13 11:57:22 +0530

    Add files via upload

---
+++ space/Q.2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void main()
+{
+	int i,j,k;
+	
+	for(i=1; i<=5; i++)
+	{
+		for(k=i;k<=4;k++)
+		{
+			printf(" ");
+		}
+		for(j=i; j>=1;j--)
+		{
+			printf("%d",j);
+				
+		}
+		printf("\n");
+	}
+	
+}
