9ff72e28856f0647c0e7843adeae27efee68c167
Sagar Verma <8737sagar@gmail.com>
2024-08-22 11:11:26 +0530

    Update Q.4.c

--- array/Q.4.c
+++ array/Q.4.c
@@ -2,22 +2,36 @@
 
 void main()
 {
-	int i,n,sum=0;
-	printf("enter the size of array :");
+	int i,n;
+	
+	printf("Enter The Size Of Array : ");
 	scanf("%d",&n);
 	
-	int a[n];
+	int a[n],b[n],c[n];
 	
-	for(i=0; i<n; i++)
+	for(i=0; i<=n-1; i++)
 	{
-		printf("enter your value of a[%d]:",i);
+		printf("Enter The Value Of a[%d] : ",i);
 		scanf("%d",&a[i]);
 	}
 	
+	printf("\n");
 	
-	for(i=0; i<n; i++)
+	for(i=0; i<=n-1; i++)
 	{
-	  sum=sum+a[i];	
+		printf("Enter The Value Of b[%d] : ",i);
+		scanf("%d",&b[i]);
 	}
-	printf("sum is %d",sum);
+	
+	printf("\n");
+	
+	for(i=0; i<=n-1; i++)
+	{
+		c[i] = a[i] + b[i];
+    }  
+	
+	for(i=0; i<=n-1; i++)
+	{
+		printf("c[%d] = %d\n ",i,c[i]);
+    }  	
 }
